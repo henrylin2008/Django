15cc6dfddaea93b9a73e0c25a8c3043cce2b792f
Henry Lin <henry.lin@appdirect.com>
2019-01-30 11:18:16 -0800

    update

--- ProTwo/ProTwo/AppTwo/views.py
+++ ProTwo/ProTwo/AppTwo/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from AppTwo.models import Topic,Webpage,AccessRecord
 
 # Create your views here.
 def index(request): 
-	my_dict = {'insert_me':"Now i'm coming from AppTwo/index.html!"}
-	return render(request, 'AppTwo/index.html', context=my_dict)
+	webpages_list = AccessRecord.objects.order_by('date')
+	date_dict = {'access_records':webpages_list}
+	# my_dict = {'insert_me':"Now i'm coming from AppTwo/index.html!"}
+	return render(request, 'AppTwo/index.html', context=date_dict)
 
 def help(request): 
 	helpdict = {'help_insert': "HELP PAGE"}

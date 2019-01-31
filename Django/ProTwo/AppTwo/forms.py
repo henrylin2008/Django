1770bd6fcf6fbec8795ce380a0cdd9e82784e304
Henry Lin <henry.lin@appdirect.com>
2019-01-30 21:55:39 -0800

    App2

--- Django/ProTwo/AppTwo/forms.py
+++ Django/ProTwo/AppTwo/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from AppTwo.models import User
 
 
-class NewUser(forms.ModelForm): 
-	class Meta: 
+class NewUserForm(forms.ModelForm): 
+	class Meta(): 
 		model = User
 		fields = '__all__'
--- Django/ProTwo/AppTwo/views.py
+++ Django/ProTwo/AppTwo/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
-from AppTwo.models import Topic,Webpage,AccessRecord,User 
+# from django.http import HttpResponse 
+# from AppTwo.models import Topic,Webpage,AccessRecord,User 
+from AppTwo.forms import NewUserForm
 
 # Create your views here.
 def index(request): 
-	webpages_list = AccessRecord.objects.order_by('date')
-	date_dict = {'access_records':webpages_list}
+	# webpages_list = AccessRecord.objects.order_by('date')
+	# date_dict = {'access_records':webpages_list}
 	# my_dict = {'insert_me':"Now i'm coming from AppTwo/index.html!"}
-	return render(request, 'AppTwo/index.html', context=date_dict)
+	return render(request, 'AppTwo/index.html')
 
 def users(request):
-	
+
+	form = NewUserForm()
+
+	if request.method == "POST": 
+		form = NewUserForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else: 
+			print('Error form invalid')
+
+	return render(request, 'AppTwo/users.html',{'form':form})
+
 	# user_list = User.objects.order_by('first_name')
 	# user_dict = {'users':user_list}
 	# return render(request,'AppTwo/users.html',context=user_dict)

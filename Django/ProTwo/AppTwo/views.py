f2f7bd07dd5704245b23c0fabb086c7b2a6175db
Henry Lin <henry.lin@appdirect.com>
2019-01-30 20:55:30 -0800

    ProTwo

--- Django/ProTwo/AppTwo/views.py
+++ Django/ProTwo/AppTwo/views.py
@@ -10,10 +10,10 @@ def index(request):
 	return render(request, 'AppTwo/index.html', context=date_dict)
 
 def users(request):
-
-	user_list = User.objects.order_by('first_name')
-	user_dict = {'users':user_list}
-	return render(request,'AppTwo/users.html',context=user_dict)
+	
+	# user_list = User.objects.order_by('first_name')
+	# user_dict = {'users':user_list}
+	# return render(request,'AppTwo/users.html',context=user_dict)
 
 def help(request): 
 	helpdict = {'help_insert': "HELP PAGE"}
